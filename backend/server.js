48f87df725b41d3b755a3be6f5df55d635c17553
Nashid <nashidk1999@gmail.com>
2024-12-28 12:53:53 +0530

    backend completed signin,logout,login

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 import express from 'express';
-const app = express();
+
 import authRoutes from './routes/auth.route.js'
+import { ENV_VARS } from './config/envVars.js';
+import {connectDB} from './config/db.js'
+const app = express();
+const PORT = ENV_VARS.PORT
+
+app.use(express.json())
 
 app.use("/api/v1/auth",authRoutes)
 
-app.listen(5000,()=>{
-    console.log(`server is running at http://localhost:5000`);
-    
+app.listen(PORT,()=>{
+    console.log(`server is running at http://localhost:`+PORT);
+    connectDB();
 })
+
+
+
+
+
