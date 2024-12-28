c6c46e4c136c39acda59165ac7922bd5cd0c6663
Nashid <nashidk1999@gmail.com>
2024-12-28 14:37:51 +0530

    added trending,trailers,details,similar backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
-import authRoutes from './routes/auth.route.js'
+import authRoutes from './routes/auth.route.js';
+import movieRoutes from './routes/movie.route.js';
 import { ENV_VARS } from './config/envVars.js';
 import {connectDB} from './config/db.js'
 const app = express();
@@ -9,6 +10,8 @@ const PORT = ENV_VARS.PORT
 app.use(express.json())
 
 app.use("/api/v1/auth",authRoutes)
+app.use("/api/v1/movie",movieRoutes)
+
 
 app.listen(PORT,()=>{
     console.log(`server is running at http://localhost:`+PORT);
