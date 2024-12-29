093d1f135b032ecb2c230b0e7e1033e9bb114b3a
Nashid <nashidk1999@gmail.com>
2024-12-29 13:06:10 +0530

    added search

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import movieRoutes from './routes/movie.route.js';
 import tvRoutes from './routes/tv.route.js';
+import searchRoutes from './routes/search.route.js'
 import {protectRoute} from './middleware/protectRoute.js'
 
 import { ENV_VARS } from './config/envVars.js';
@@ -17,6 +18,8 @@ app.use(cookieParser());
 app.use("/api/v1/auth",authRoutes)
 app.use("/api/v1/movie",protectRoute,movieRoutes)
 app.use("/api/v1/tv",protectRoute,tvRoutes)
+app.use("/api/v1/search",protectRoute,searchRoutes)
+
 
 
 
