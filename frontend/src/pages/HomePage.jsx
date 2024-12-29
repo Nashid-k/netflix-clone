30c323d074b405f8ae7dd2ddb0815bea902129e5
Nashid <nashidk1999@gmail.com>
2024-12-29 13:44:49 +0530

    frontend initial commit

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,6 @@
+
+export const HomePage = () => {
+  return (
+    <div className="hero-bg h-screen">HomePage</div>
+  )
+}
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+export const SignUpPage = () => {
+  return (
+    <div className="h-screen w-full hero-bg">
+      <header className="max-w-6xl mx-auto flex items-center justify-between p-4">
+      <Link to={"/"}>
+      <img src="/netflix-logo.png" alt="logo" className="w-52"/>
+      </Link>
+      </header>
+
+      <div></div>
+    </div>
+  )
+}
