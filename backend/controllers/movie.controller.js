3d1a3e8e1c4dc5b2baa5cd30b3fbd591e3b780d9
Nashid <nashidk1999@gmail.com>
2024-12-29 10:47:17 +0530

    added getmoviesBycategory,tvRoutes

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -46,3 +46,13 @@ export async function getSimilarMovies(req,res) {
     res.status(500).json({success:false,message:"Internal Server Error"})
   }
 }
+
+export async function getMoviesByCategory(req,res) {
+  const {category} = req.params;
+  try {
+    const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`);
+    res.status(200).json({success:true,content:data.results})
+  } catch (error) {
+    res.status(500).json({success:false,message:"Internal Server Error"})
+  }
+}
